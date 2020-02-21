6492cda5bfe9490989d4f7a3c329810cd1a94b9d
TREY <hugh.a.lloyd@gmail.com>
2020-02-21 10:53:46 -0600

    setup

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,5 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
 import "./index.css";
 import App from "./components/App";
 
